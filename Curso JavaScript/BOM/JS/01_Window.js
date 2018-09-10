d1f86f7397efb44040b55c4bd0659185cbe99fa5
610rodrigo <rodrigo.fla75@gmail.com>
2018-09-10 17:15:24 -0300

    File Window added directory BOM

---
+++ Curso JavaScript/BOM/JS/01_Window.js
@@ -0,0 +1,5 @@
+var w = window.innerWidth || document.body.clientWidth;
+var h = window.innerHeight || document.body.clientHeight;
+
+var x = document.getElementById("demo");
+x.innerHTML = "Browser inner window width: " + w + ", height: " + h + ".";
