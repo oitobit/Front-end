972d9923a9fa44859b980d16b02a41851b8d1f48
610rodrigo <rodrigo.fla75@gmail.com>
2018-10-24 12:36:01 -0300

    File navigator added to repository Front-end

---
+++ Curso JavaScript/BOM/JS/05_Navigator.js
@@ -0,0 +1,7 @@
+document.getElementById("demo").innerHTML = window.navigator.cookieEnabled;
+
+document.getElementById("demo2").innerHTML = window.navigator.appName;
+
+document.getElementById("demo3").innerHTML = window.navigator.appCodeName;
+
+document.getElementById("demo4").innerHTML = window.navigator.userAgent;
